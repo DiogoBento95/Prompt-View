cb595dfb38fde4e2fad2f9bf8906ab92a62d265c
DiogoTB <diogotb10@hotmail.com>
2021-02-26 00:26:45 -0100

    extra features

--- src/promptViewTest/PromptViewExercise.java
+++ src/promptViewTest/PromptViewExercise.java
@@ -1,25 +1,44 @@
 package promptViewTest;
 
 import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
 import org.academiadecodigo.bootcamp.scanners.string.PasswordInputScanner;
 import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
 
 public class PromptViewExercise {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-            Prompt prompt = new Prompt(System.in,System.out);
+        Prompt prompt = new Prompt(System.in, System.out);
 
-        StringInputScanner question1 = new StringInputScanner();
-        question1.setMessage("What is your Username?");
+        while (true) {
 
-        PasswordInputScanner question2 = new PasswordInputScanner();
-        question2.setMessage("What is your Password?");
+            String[] options = {"Login", "Exit"};
 
-        String username = prompt.getUserInput(question1);
-        String password = prompt.getUserInput(question2);
+            MenuInputScanner menu = new MenuInputScanner(options);
+            menu.setMessage("What would you like to do?");
 
-        System.out.println("Hello " + username + ", how are you today?");
+            int optionsChoice = prompt.getUserInput(menu);
 
+            if (optionsChoice == 1) {
+
+                System.out.println("Insert your Username and Password below: ");
+
+                StringInputScanner question1 = new StringInputScanner();
+                question1.setMessage("What is your Username? : ");
+
+                PasswordInputScanner question2 = new PasswordInputScanner();
+                question2.setMessage("What is your Password? : ");
+
+                String username = prompt.getUserInput(question1);
+                String password = prompt.getUserInput(question2);
+
+                System.out.println("Login authorized.");
+                System.out.println("Hello " + username + ", how are you today?");
+
+            } else {
+                System.exit(0);
+            }
+        }
     }
 }
