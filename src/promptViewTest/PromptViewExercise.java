6210e9d676e60f6f27072dea75598298c82442b5
DiogoTB <diogotb10@hotmail.com>
2021-02-26 00:01:39 -0100

    the complete exercise

--- src/promptViewTest/PromptViewExercise.java
+++ src/promptViewTest/PromptViewExercise.java
@@ -1,12 +1,25 @@
 package promptViewTest;
 
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.string.PasswordInputScanner;
+import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
+
 public class PromptViewExercise {
 
     public static void main(String[] args){
 
+            Prompt prompt = new Prompt(System.in,System.out);
 
+        StringInputScanner question1 = new StringInputScanner();
+        question1.setMessage("What is your Username?");
 
+        PasswordInputScanner question2 = new PasswordInputScanner();
+        question2.setMessage("What is your Password?");
 
+        String username = prompt.getUserInput(question1);
+        String password = prompt.getUserInput(question2);
+
+        System.out.println("Hello " + username + ", how are you today?");
 
     }
 }
